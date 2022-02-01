e9ea78232a1bd187dc9d1909c5c313822e595d41
Vadym-M <65238855+Vadym-M@users.noreply.github.com>
2022-02-01 23:19:07 +0100

    update MainActivity.kt

--- app/src/main/java/com/vinade/starwars/view/activities/MainActivity.kt
+++ app/src/main/java/com/vinade/starwars/view/activities/MainActivity.kt
@@ -2,21 +2,48 @@ package com.vinade.starwars.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.vinade.starwars.R
+import com.vinade.starwars.databinding.ActivityMainBinding
 import com.vinade.starwars.repository.StarWarsRepository
+import com.vinade.starwars.view.fragments.FavoriteFragment
+import com.vinade.starwars.view.fragments.HomeFragment
 import com.vinade.starwars.viewmodel.StarWarsViewModel
 import com.vinade.starwars.viewmodel.ViewModelFactory
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
     lateinit var viewModel: StarWarsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-       val repo = StarWarsRepository()
-        viewModel = ViewModelProvider(this, ViewModelFactory(repo)).get(StarWarsViewModel::class.java)
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> {
+                    supportFragmentManager.commit {
+                        replace(R.id.fragmentContainer, HomeFragment())
+                    }
+                 true
+                }
+                R.id.favorite -> {
+                    supportFragmentManager.commit {
+                        replace(R.id.fragmentContainer, FavoriteFragment())
+                    }
+                 true
+                }
+                else -> false
+            }
+        }
+        if(savedInstanceState == null){
+            binding.bottomNavigation.selectedItemId = R.id.home
+        }
+
+
     }
 }
