54a029c12807c3874e2d497b903b852c2688c435
Vadym-M <65238855+Vadym-M@users.noreply.github.com>
2022-02-01 20:44:26 +0100

    update MainActivity.kt

--- app/src/main/java/com/vinade/starwars/MainActivity.kt
+++ app/src/main/java/com/vinade/starwars/MainActivity.kt
@@ -2,10 +2,20 @@ package com.vinade.starwars
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.vinade.starwars.repository.StarWarsRepository
+import com.vinade.starwars.viewmodel.StarWarsViewModel
+import com.vinade.starwars.viewmodel.ViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: StarWarsViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       val repo = StarWarsRepository()
+        viewModel = ViewModelProvider(this, ViewModelFactory(repo)).get(StarWarsViewModel::class.java)
     }
 }
