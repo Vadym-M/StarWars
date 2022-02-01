51c262038ee49612d92c0d140b4915dc29d1902c
Vadym-M <65238855+Vadym-M@users.noreply.github.com>
2022-02-01 20:52:59 +0100

    add fragments

--- app/src/main/java/com/vinade/starwars/view/activities/MainActivity.kt
+++ app/src/main/java/com/vinade/starwars/view/activities/MainActivity.kt
@@ -1,8 +1,9 @@
-package com.vinade.starwars
+package com.vinade.starwars.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import com.vinade.starwars.R
 import com.vinade.starwars.repository.StarWarsRepository
 import com.vinade.starwars.viewmodel.StarWarsViewModel
 import com.vinade.starwars.viewmodel.ViewModelFactory
