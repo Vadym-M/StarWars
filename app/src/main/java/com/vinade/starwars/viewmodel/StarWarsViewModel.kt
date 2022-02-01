65849b008f584651a33ecec3e00af78c107df856
Vadym-M <65238855+Vadym-M@users.noreply.github.com>
2022-02-01 23:22:16 +0100

    added adapter

--- app/src/main/java/com/vinade/starwars/viewmodel/StarWarsViewModel.kt
+++ app/src/main/java/com/vinade/starwars/viewmodel/StarWarsViewModel.kt
@@ -1,15 +1,19 @@
 package com.vinade.starwars.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.vinade.starwars.model.Result
 import com.vinade.starwars.model.StarWarsResult
 import com.vinade.starwars.repository.StarWarsRepository
 import kotlinx.coroutines.launch
 import retrofit2.Response
 
 class StarWarsViewModel(private val repository: StarWarsRepository) : ViewModel() {
-    private var _result = MutableLiveData<Response<StarWarsResult>>()
+    private var _result = MutableLiveData<List<Result>>()
+    val result: LiveData<List<Result>>
+    get() = _result
 
     init {
         getPeoplePage()
@@ -17,7 +21,7 @@ class StarWarsViewModel(private val repository: StarWarsRepository) : ViewModel(
     private fun getPeoplePage(){
         viewModelScope.launch {
                 val response = repository.getPeoplePage()
-                _result.postValue(response)
+                _result.postValue(response.body()!!.results)
 
 
         }
