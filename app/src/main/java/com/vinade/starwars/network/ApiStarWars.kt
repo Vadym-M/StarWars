34dca1d11e8f65dbec0bcc5317730c49d08d8277
Vadym-M <65238855+Vadym-M@users.noreply.github.com>
2022-02-07 18:00:40 +0100

    changed query (get/ retrofit)

--- app/src/main/java/com/vinade/starwars/network/ApiStarWars.kt
+++ app/src/main/java/com/vinade/starwars/network/ApiStarWars.kt
@@ -3,8 +3,9 @@ package com.vinade.starwars.network
 import com.vinade.starwars.model.StarWarsResult
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiStarWars {
-    @GET("people/?page=1")
-    suspend fun getPeoplePage():Response<StarWarsResult>
+    @GET("people/")
+    suspend fun getPeoplePage(@Query("page") page: Int):Response<StarWarsResult>
 }
