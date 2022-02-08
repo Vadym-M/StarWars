a1027fc7d3172d753383535e93b133eeac1fe595
Vadym-M <65238855+Vadym-M@users.noreply.github.com>
2022-02-08 14:35:09 +0100

    updated navigation

--- app/src/main/java/com/vinade/starwars/view/activities/MainActivity.kt
+++ app/src/main/java/com/vinade/starwars/view/activities/MainActivity.kt
@@ -3,62 +3,82 @@ package com.vinade.starwars.view.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
-import androidx.lifecycle.ViewModelProvider
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.vinade.starwars.R
 import com.vinade.starwars.databinding.ActivityMainBinding
-import com.vinade.starwars.repository.StarWarsRepository
+import com.vinade.starwars.model.Result
+import com.vinade.starwars.util.Constants.Companion.DETAIL_FRAGMENT
+import com.vinade.starwars.util.Constants.Companion.FAVORITE_FRAGMENT
+import com.vinade.starwars.util.Constants.Companion.HOME_FRAGMENT
+import com.vinade.starwars.util.Navigator
 import com.vinade.starwars.view.fragments.DetailFragment
 import com.vinade.starwars.view.fragments.FavoriteFragment
 import com.vinade.starwars.view.fragments.HomeFragment
-import com.vinade.starwars.viewmodel.StarWarsViewModel
-import com.vinade.starwars.viewmodel.ViewModelFactory
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Navigator {
 
     lateinit var binding: ActivityMainBinding
-    lateinit var viewModel: StarWarsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        if(savedInstanceState == null){
-            //binding.bottomNavigation.selectedItemId = R.id.home
-            supportFragmentManager.commit {
-                replace(R.id.fragmentContainer, HomeFragment())
-            }
+        if (savedInstanceState == null) {
+            showFragment(HomeFragment(), null, HOME_FRAGMENT)
         }
 
 
         binding.bottomNavigation.setOnItemSelectedListener {
-            when(it.itemId){
+            when (it.itemId) {
                 R.id.home -> {
-                    if(binding.bottomNavigation.selectedItemId != R.id.home){
-                        supportFragmentManager.commit {
-                            replace(R.id.fragmentContainer, HomeFragment())
-                        }
+                    if (binding.bottomNavigation.selectedItemId != R.id.home) {
+                        showFragment(HomeFragment(), null, HOME_FRAGMENT)
                     }
                     true
                 }
                 R.id.favorite -> {
-                    if (binding.bottomNavigation.selectedItemId != R.id.favorite){
-                        supportFragmentManager.commit {
-                            replace(R.id.fragmentContainer, FavoriteFragment())}
+                    if (binding.bottomNavigation.selectedItemId != R.id.favorite) {
+                        showFragment(FavoriteFragment(), null, FAVORITE_FRAGMENT)
                     }
                     true
                 }
                 else -> false
             }
         }
-        fun hideBottomBar(){
-            binding.bottomNavigation.visibility = View.GONE
+
+
+    }
+
+    private fun hideBottomBar() {
+        binding.bottomNavigation.visibility = View.GONE
+    }
+
+    private fun showBottomBar() {
+        binding.bottomNavigation.visibility = View.VISIBLE
+    }
+
+    override fun showDetailFragment(result: Result) {
+        val fragment = DetailFragment.newInstance(result)
+        showFragment(fragment, "bs", DETAIL_FRAGMENT)
+        hideBottomBar()
+    }
+
+    private fun showFragment(fragment: Fragment, backStack: String?, tag: String?) {
+        supportFragmentManager.commit {
+            backStack?.let { addToBackStack(backStack) }
+            replace(R.id.fragmentContainer, fragment, tag)
+        }
+    }
+
+    override fun onBackPressed() {
+        val fragment = supportFragmentManager.findFragmentByTag(DETAIL_FRAGMENT)
+        fragment?.let {
+            if (fragment.isVisible)
+                showBottomBar()
         }
 
-
-
+        super.onBackPressed()
     }
 }
