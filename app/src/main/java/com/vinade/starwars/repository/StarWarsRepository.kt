94582872da987159dc59cc244902c3ac4d9cd1fd
Vadym-M <65238855+Vadym-M@users.noreply.github.com>
2022-02-10 17:28:20 +0100

    updated repository

--- app/src/main/java/com/vinade/starwars/repository/StarWarsRepository.kt
+++ app/src/main/java/com/vinade/starwars/repository/StarWarsRepository.kt
@@ -1,12 +1,11 @@
 package com.vinade.starwars.repository
 
-import com.vinade.starwars.model.Result
 import com.vinade.starwars.network.RetrofitClient
-import com.vinade.starwars.room.StarWarsDao
-
 
 class StarWarsRepository : Repository{
 
     suspend fun getPeoplePage(page: Int) = RetrofitClient.apiStarWars.getPeoplePage(page)
+
+    suspend fun getPeopleByQuery(query: String) = RetrofitClient.apiStarWars.getPeopleByQuery(query)
 
 }
